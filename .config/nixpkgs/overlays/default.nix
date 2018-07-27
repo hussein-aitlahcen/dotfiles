702dfd756af386825ee5be4d6a34171b0c66b5fa
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-07-27 17:08:08 +0200

    add: haskell ide engine

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -1,5 +1,13 @@
 self: super:
-
 {
-  emacs26      = super.callPackage ./pkgs/emacs {};
+  # Newer emacs
+  emacs26 = super.callPackage ./pkgs/emacs {};
+
+  # Haskell IDE Engine
+  hies = import (super.fetchFromGitHub {
+           owner  = "domenkozar";
+           repo   = "hie-nix";
+           rev    = "e3113da93b479bec3046e67c0123860732335dd9";
+           sha256 = "05rkzjvzywsg66iafm84xgjlkf27yfbagrdcb8sc9fd59hrzyiqk";
+         });
 }
