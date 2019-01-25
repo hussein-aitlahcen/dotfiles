6183fa5ae8657fae72040ecd5dad12aa43266379
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2019-01-25 17:06:46 +0100

    fix: update

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -7,7 +7,7 @@ self: super:
     src = super.fetchgit {
       url = "https://git.savannah.gnu.org/git/emacs.git";
       rev = "96e386e60de1d4ae9d702fc376ef7a9279a6aa66";
-      sha256 = "sha256:0b6k1wq44rc8gkvxhi1bbjxbz3cwg29qbq8mklq2az6p1hjgrx00";
+      sha256 = "sha256:11k5ldi33cq1dk3w9i11cr9xl5mc7f5mnxpvqlzsnm6gfijz4cas";
     };
   });
 }
