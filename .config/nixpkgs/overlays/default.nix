16ad441131605d2285ceaac501d3f14c435b4eb4
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2019-01-25 16:24:07 +0100

    fix: update

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -4,7 +4,7 @@ self: super:
     name = "emacs-${version}${versionModifier}";
     version = "27.1";
     versionModifier = "";
-    src = fetchurl {
+    src = super.fetchurl {
       url = "mirror://gnu/emacs/${name}.tar.xz";
       sha256 = "0b6k1wq44rc8gkvxhi1bbjxbz3cwg29qbq8mklq2az6p1hjgrx0w";
     };
