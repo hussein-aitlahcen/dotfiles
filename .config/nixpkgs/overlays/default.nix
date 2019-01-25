b0e2fd380c03e3501ee2552bed20541447f29748
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2019-01-25 16:23:52 +0100

    fix: update

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -1,7 +1,12 @@
 self: super:
 {
-  # custom emacs
-  emacs = super.emacs.overrideAttrs (old: {
+  emacs = super.emacs.overrideAttrs (old: rec {
+    name = "emacs-${version}${versionModifier}";
     version = "27.1";
+    versionModifier = "";
+    src = fetchurl {
+      url = "mirror://gnu/emacs/${name}.tar.xz";
+      sha256 = "0b6k1wq44rc8gkvxhi1bbjxbz3cwg29qbq8mklq2az6p1hjgrx0w";
+    };
   });
 }
