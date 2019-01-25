8d8497d07c62137a49a38b84cf21a0454d6ae1ac
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2019-01-25 17:22:04 +0100

    fix: update

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -4,6 +4,9 @@ self: super:
     name = "emacs-${version}${versionModifier}";
     version = "27.1";
     versionModifier = "";
+    patches = [
+      ./emacs.patch
+    ];
     src = super.fetchgit {
       url = "https://git.savannah.gnu.org/git/emacs.git";
       rev = "96e386e60de1d4ae9d702fc376ef7a9279a6aa66";
