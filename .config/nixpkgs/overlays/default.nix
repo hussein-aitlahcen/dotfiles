d7da6c1e82c0dc58a246cdfd188878864f039a6a
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2019-01-25 16:27:17 +0100

    fix: update

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -6,7 +6,7 @@ self: super:
     versionModifier = "";
     src = super.fetchurl {
       url = "mirror://gnu/emacs/${name}.tar.xz";
-      sha256 = "0b6k1wq44rc8gkvxhi1bbjxbz3cwg29qbq8mklq2az6p1hjgrx0w";
+      sha256 = "0b6k1wq44rc8gkvxhi1bbjxbz3cwg29qbq8mklq2az6p1hjgrx00";
     };
   });
 }
